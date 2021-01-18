9757b54b75f7dc15e37235461a026a76a19d80ff
Bill Theocharoulas <Bill.Theocharoulas@ibm.com>
2021-01-17 19:21:15 -0500

    added formula for operator-sdk 1.1.0

---
+++ operator-sdk-v1.1.0.rb
@@ -0,0 +1,42 @@
+class OperatorSdk < Formula
+    desc "SDK for building Kubernetes applications"
+    homepage "https://coreos.com/operators/"
+    url "https://github.com/operator-framework/operator-sdk.git",
+        tag:      "v1.1.0",
+        revision: "9d27e224efac78fcc9354ece4e43a50eb30ea968"
+    license "Apache-2.0"
+    head "https://github.com/operator-framework/operator-sdk.git"
+  
+    bottle do
+      cellar :any_skip_relocation
+      sha256 "69874324c44d8731c7a0c572a1dbcbea2b27811ffd5d29d60f3c32ae46434287" => :catalina
+      sha256 "88d91481811b2fba02c348d54a6f46b6303fccdd60b6a36ee4f56412a773698e" => :mojave
+      sha256 "e2181db5b4f393784088f66377fe1fb410002e1faac0802e115209805ee9b064" => :high_sierra
+    end
+  
+    depends_on "go"
+  
+    def install
+      ENV["GOBIN"] = bin
+      system "make", "install"
+  
+      # Install bash completion
+      output = Utils.safe_popen_read("#{bin}/operator-sdk", "completion", "bash")
+      (bash_completion/"operator-sdk").write output
+  
+      # Install zsh completion
+      output = Utils.safe_popen_read("#{bin}/operator-sdk", "completion", "zsh")
+      (zsh_completion/"_operator-sdk").write output
+    end
+  
+    test do
+      if build.stable?
+        version_output = shell_output("#{bin}/operator-sdk version")
+        assert_match "version: \"v#{version}\"", version_output
+        assert_match stable.specs[:revision], version_output
+      end
+  
+      system bin/"operator-sdk", "init", "--domain=example.com", "--repo=example.com/example/example"
+      assert_predicate testpath/"bin/manager", :exist?
+    end
+  end
